eb5880886d3e993e599c30c4da65807276f44b0b
mohamed <mnafe44@gmail.com>
2026-01-08 17:06:01 +0200

    creating the project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LoginAndRegisterApp());
+}
+
+class LoginAndRegisterApp extends StatelessWidget {
+  const LoginAndRegisterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.dark(),
+    );
+  }
+}
